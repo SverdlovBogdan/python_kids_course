53b02839c5675e57c86b2b42c075e7aabaa3f770
Sverdlov <BSverdlov@kiev.luxoft.com>
2018-09-19 13:58:35 +0300

    Incapsulate sweep and ball in classes

--- radar.py
+++ radar.py
@@ -4,16 +4,14 @@ import math
 # --- Set up the constants
 
 # Size of the screen
-SCREEN_WIDTH = 200
-SCREEN_HEIGHT = 200
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
 CENTER_X = SCREEN_WIDTH // 2
 CENTER_Y = SCREEN_HEIGHT // 2
-RADIANS_PER_FRAME = 0.01
-SWEEP_LENGTH = 250
-
-ANGLE = 0.0
 
 # (x - x1) * (y2 - y1) - (x2 - x1) * (y - y1) = 0
+
+# if check_point(CENTER_X, CENTER_Y, int(x), int(y), ball.x, ball.y)
 
 def check_point(x1, y1, x2, y2, x, y):
     val = (x - x1) * (y2 - y1) - (x2 - x1) * (y - y1)
@@ -28,7 +26,36 @@ class Ball:
         self.x = 0
         self.y = 0
         self.size = 0
-        self.draw = False
+        self.visible = False
+
+    def draw(self):
+        arcade.draw_circle_filled(self.x, self.y, self.size, self.color)
+
+    def update():
+        pass
+
+class Sweep:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.sweep_length = 250
+        self.angle = 0.0
+        self.angle_per_frame = 0.01
+
+    def draw(self):
+        # Draw the outline of the radar
+        arcade.draw_circle_outline(CENTER_X, CENTER_Y, self.sweep_length,
+                                   arcade.color.DARK_GREEN, 10)
+
+        # Draw the line of the radar.
+        arcade.draw_line(CENTER_X, CENTER_Y, self.x, self.y, arcade.color.OLIVE, 4)
+
+    def update(self):
+        self.angle += self.angle_per_frame
+
+        # Calculate the end point of our radar sweep. Using math.
+        self.x = self.sweep_length * math.sin(self.angle) + CENTER_X
+        self.y = self.sweep_length * math.cos(self.angle) + CENTER_Y
 
 
 def make_ball(x, y):
@@ -57,6 +84,7 @@ class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Bouncing Balls Demo")
         self.ball_list = []
+        self.sweep = Sweep()
 
     def on_draw(self):
         """
@@ -66,36 +94,21 @@ class MyGame(arcade.Window):
         # This command has to happen before we start drawing
         arcade.start_render()
 
-
-        # Put the text on the screen.
         output = "Balls: {}".format(len(self.ball_list))
         arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
 
-        global ANGLE
-        ANGLE += RADIANS_PER_FRAME
-
-        # Calculate the end point of our radar sweep. Using math.
-        x = SWEEP_LENGTH * math.sin(ANGLE) + CENTER_X
-        y = SWEEP_LENGTH * math.cos(ANGLE) + CENTER_Y
-        # print(int(x), int(y))
-
-        # Draw the radar line
-        arcade.draw_line(CENTER_X, CENTER_Y, x, y, arcade.color.OLIVE, 4)
-
-        # Draw the outline of the radar
-        arcade.draw_circle_outline(CENTER_X, CENTER_Y, SWEEP_LENGTH,
-                                arcade.color.DARK_GREEN, 10)
+        self.sweep.draw()
 
         for ball in self.ball_list:
-            if check_point(CENTER_X, CENTER_Y, int(x), int(y), ball.x, ball.y):
-                arcade.draw_circle_filled(ball.x, ball.y, ball.size, ball.color)
-                ball.draw = True
-            elif ball.draw:
-                arcade.draw_circle_filled(ball.x, ball.y, ball.size, ball.color)
+            if ball.visible:
+                ball.draw()
 
     def update(self, delta_time):
-        """ Movement and game logic """
-        pass
+        self.sweep.update()
+
+        for ball in self.ball_list:
+            if check_point(CENTER_X, CENTER_Y, self.sweep.x, self.sweep.y, ball.x, ball.y):
+                ball.visible = True
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
         """
@@ -103,7 +116,7 @@ class MyGame(arcade.Window):
         """
         print(x, y)
 
-    
+
     def on_mouse_release(self, x, y, button, modifiers):
         """
         Called when a user releases a mouse button.
