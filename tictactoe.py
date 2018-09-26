0df763edcc4e1f49d175e8b9a9d894708e68a3d2
bsverdlov <freezar92@gmail.com>
2018-09-26 16:27:14 +0300

    TicTacToe lesson 2

--- tictactoe.py
+++ tictactoe.py
@@ -7,12 +7,38 @@ def print_board():
     print('- - -')
     print(board[6] + '|' + board[7] + '|' + board[8])
 
-def player_chose():
-    number = int(input('Введите номер ячейки: '))
-    board[number - 1] = 'X'
+def player_x_choose():
+    all_numbers = '1 2 3 4 5 6 7 8 9'
 
+    while True:
+        number = input('Введите номер ячейки для X: ')
+        
+        if number not in all_numbers or board[int(number) - 1] not in all_numbers:
+            print('Неверный номер ячейки или ячейка уже занята')
+            continue
+        else:
+            board[int(number) - 1] = 'O'
+            break
+
+    board[int(number) - 1] = 'X'
+
+def player_o_choose():
+    all_numbers = '1 2 3 4 5 6 7 8 9'
+    
+    while True:
+        number = input('Введите номер ячейки для О: ')
+
+        if number not in all_numbers or board[int(number) - 1] not in all_numbers:
+            print('Неверный номер ячейки или ячейка уже занята')
+            continue
+        else:
+            board[int(number) - 1] = 'O'
+            break
 
 print_board()
-player_chose()
+player_x_choose()
 print_board()
+player_o_choose()
+print_board()
+
 
